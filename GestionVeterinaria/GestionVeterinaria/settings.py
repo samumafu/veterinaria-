543f9b192fcd6955195f13ec093398818e2d7065
Johan <delgadodelgad17@gmail.com>
2025-05-27 19:14:59 -0500

    correcion

--- GestionVeterinaria/GestionVeterinaria/settings.py
+++ GestionVeterinaria/GestionVeterinaria/settings.py
@@ -44,17 +44,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'GestionVeterinaria.urls'
 
